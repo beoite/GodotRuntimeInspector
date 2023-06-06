bec8ad1f8c3efabbcfb9ff3610050fa1158e77d4
user <user@localhost>
2023-06-06 01:54:25 +0100

    dev

--- GodotRunTimeInspector/Scripts/GodotRuntimeInspector.cs
+++ GodotRunTimeInspector/Scripts/GodotRuntimeInspector.cs
@@ -78,7 +78,7 @@ namespace GodotRuntimeInspector.Scripts
                 SceneManager.Init();
                 // Add the node as a child of the node the script is attached to.
                 AddChild(SceneManager.SimpleCameraNode);
-                AddChild(SceneManager.TestNode);
+                //AddChild(SceneManager.TestNode);
                 TestCubes.Create(this);
             }
         }
