3f6d5f0274cf28a5700e4df08b764f3e29a92a81
user <user@localhost>
2025-03-19 22:42:08 +0000

    no message

--- addons/GodotRuntimeInspector/Scripts/Config.cs
+++ addons/GodotRuntimeInspector/Scripts/Config.cs
@@ -5,7 +5,6 @@
         public static Godot.Key EnabledKey = Godot.Key.F1;
         public static bool Enabled = true;
         public static float MinRowHeight = 25f;
-        public static bool ShowDemoWindow = false;
         public static uint InputTextMaxLength = 64;
         public static float VectorDragSpeed = 0.001f;
     }
